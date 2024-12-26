d820d7fe1045949053de09425cb869948ddb26b8
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-12-26 09:23:42 +0530

    random color change

--- src/components/user/Users.jsx
+++ src/components/user/Users.jsx
@@ -1,11 +1,26 @@
-// src/components/view/User.js
-import React from 'react';
-function Users() {
+import React, { useState,useEffect } from 'react'
+const User = () => {
+    const [bgColor, setBgColor] = useState('white');
+    useEffect(() => {
+      const inSideBgColor = setInterval(() => {
+          setBgColor(getRandomColor());
+      }, 1000);
+      return () => clearInterval(inSideBgColor)
+    }, [bgColor])
+    const getRandomColor = () => {
+        const colorLetters = '0123456789ABCDEF'
+        let color = '#'
+        for (let i = 0; i < 6; i++) {
+            color += colorLetters.charAt(Math.floor(Math.random() * colorLetters.length   ))
+        }
+        return color
+    }
+    useEffect(() => {
+        document.body.style.backgroundColor = bgColor;
+    })
   return (
-    <div>
-      <h1>User</h1>
+      <div className='flex items-center justify-center h-screen'>
     </div>
-  );
+  )
 }
-
-export default Users;
+export default User
